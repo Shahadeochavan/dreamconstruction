90510b1e2d30340a42a48fe0abbd41a3e51ac1d9
nikhil raskar <nikhilraskar50@gmail.com>
2017-02-09 00:43:39 -0800

    Check

--- src/main/java/com/nextech/erp/dao/ClientDao.java
+++ src/main/java/com/nextech/erp/dao/ClientDao.java
@@ -1,20 +1,8 @@
 package com.nextech.erp.dao;
 
-import java.util.List;
-
 import com.nextech.erp.model.Client;
-import com.nextech.erp.model.Vendor;
 
 public interface ClientDao extends SuperDao<Client>{
-/*	public Long addClient(Client Client) throws Exception;
-
-	public Client getClientById(long id) throws Exception;
-
-	public List<Client> getClientList() throws Exception;
-
-	public boolean deleteClient(long id) throws Exception;
-
-	public Client updateClient(Client Client) throws Exception;*/
 
 	public Client getClientByCompanyName(String companyName) throws Exception;
 

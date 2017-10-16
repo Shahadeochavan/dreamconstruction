a2ea64d1e2fa61d3c00577b978731cf7c1506c76
shahadeo <shahadeochavan@gmail.com>
2017-10-16 20:15:31 +0530

    test

--- src/main/java/com/nextech/systeminventory/controller/ProductStoreInAndOut.java
+++ src/main/java/com/nextech/systeminventory/controller/ProductStoreInAndOut.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.enterprise.inject.Produces;
 import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
--- src/main/java/com/nextech/systeminventory/daoImpl/ProductorderassociationDaoImpl.java
+++ src/main/java/com/nextech/systeminventory/daoImpl/ProductorderassociationDaoImpl.java
@@ -1,6 +1,5 @@
 package com.nextech.systeminventory.daoImpl;
 
-import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Criteria;

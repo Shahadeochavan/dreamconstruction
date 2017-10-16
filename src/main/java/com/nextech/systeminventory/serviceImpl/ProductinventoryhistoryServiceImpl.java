f718da4afbf1213cbee2d1c75f2bde1cd9ac16a0
shahadeo <shahadeochavan@gmail.com>
2017-10-16 20:46:50 +0530

    test

--- src/main/java/com/nextech/systeminventory/serviceImpl/ProductinventoryhistoryServiceImpl.java
+++ src/main/java/com/nextech/systeminventory/serviceImpl/ProductinventoryhistoryServiceImpl.java
@@ -7,5 +7,4 @@ import com.nextech.systeminventory.service.ProductinventoryhistoryService;
 @Service
 public class ProductinventoryhistoryServiceImpl extends CRUDServiceImpl<Productinventoryhistory> implements
 		ProductinventoryhistoryService {
-
 }

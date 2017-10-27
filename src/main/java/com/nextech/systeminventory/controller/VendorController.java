77d26e07b2b00914f0adf74650c5d982c88cd612
shahadeo <shahadeochavan@gmail.com>
2017-10-27 14:19:30 +0530

    chages in vendor

--- src/main/java/com/nextech/systeminventory/controller/VendorController.java
+++ src/main/java/com/nextech/systeminventory/controller/VendorController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.nextech.systeminventory.constants.ERPConstants;
 import com.nextech.systeminventory.dto.VendorDTO;
 import com.nextech.systeminventory.factory.VendorFactory;

4d4c6407363eefc959f18d4a7de266acd3953964
shahadeo <shahadeochavan@gmail.com>
2017-12-05 16:15:53 +0530

    add new file

---
+++ src/main/java/com/nextech/dreamConstruction/dto/ProductAssoParts.java
@@ -0,0 +1,18 @@
+package com.nextech.dreamConstruction.dto;
+
+import com.nextech.dreamConstruction.model.Product;
+
+public class ProductAssoParts {
+	
+	private Product product;
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+
+}
